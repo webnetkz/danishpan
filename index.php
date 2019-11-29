4fa9ffafbf80067d1e4a6ea3034ced8b357cd094
webnetkz <8gg001@gmail.com>
2019-11-29 18:10:51 +0600

    append header

---
+++ index.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <link rel="stylesheet" href="public/css/style.css">
+    <link rel="stylesheet" href="public/css/headerPs.css">
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+
+    <title>Danishpan</title>
+</head>
+<body>
+    <div class="wrapper">
+
+        <header>
+            <h1>
+                DANISHPAN.KZ
+            </h1>
+            <p>+7 (705) 588 12 10</p>
+            <p>info@danishpan.kz</p>
+            <h3>
+                Республикалық қашықтық байқаулар орталығы 
+            </h3>
+            <h3>
+                Республиканский центр дистанционных конкурсов
+            </h3>
+            <menu>
+                <a href="" class="menuItem active">
+                    Негізгі бет
+                    <hr class="active">
+                    Главная
+                </a>
+                <a href="" class="menuItem">
+                    Байқаулар
+                    <hr>
+                    Конкурсы
+                </a>
+                <a href="" class="menuItem">
+                    Мұрағат
+                    <hr>
+                    Архив
+                </a>
+                <a href="" class="menuItem">
+                    Өтінім үлгісі
+                    <hr>
+                    Форма заявки
+                </a>
+                <a href="" class="menuItem">
+                    Байланыс
+                    <hr>
+                    Контакты
+                </a>
+            </menu>
+        </header>
+        
+        <div class="content">
+            123
+        </div>
+
+        <footer>
+            123
+        </footer>
+
+    </div>
+</body>
+</html>
